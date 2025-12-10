9fbbbafd5a69e783da58b2aa17ce54d2367e0c65
ashkar2023 <muhammedashkar2023@gmail.com>
2025-12-10 10:27:08 +0530

    fix: prod env config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    backendBaseUrl: 'https://eksl.in'
+    backendBaseUrl: 'https://api.eksl.in', // Update this with your actual backend URL
+    s3ApiBaseUrl: "https://skemmarize.s3.ap-south-1.amazonaws.com"
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,4 +3,3 @@ export const environment = {
     backendBaseUrl: 'http://localhost:8080', // Update this with your actual backend URL
     s3ApiBaseUrl: "https://skemmarize.s3.ap-south-1.amazonaws.com"
 };
-
